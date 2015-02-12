8b2b8d526011d3584dff1c371c39b1ccd248493a
Brad J <bivvo.accounts@gmail.com>
2015-02-11 22:22:59 -0800

    config please
    Adding Configuration.

--- Controllers/SimpleController.cs
+++ Controllers/SimpleController.cs
@@ -1,16 +1,36 @@
 using Microsoft.AspNet.Mvc;
 using MvcSample.Web.Models;
 using Microsoft.AspNet.Routing;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.AspNet.Hosting;
 
 namespace ssdugpresentation.Controllers
 {
     [Route("Simple")]
     public class SomeSuperLongNameThatYouDoNotWishToHaveAsAnActualRouteController : Controller
     {
+        private IConfiguration _config;
+
+        public SomeSuperLongNameThatYouDoNotWishToHaveAsAnActualRouteController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
         [Route("Index")]
         public IActionResult SomeCrazyActionNameIWouldNotActualUseAndResolveBackToIndex()
         {
             return View(User());
+        }
+
+        [Route("Config")]
+        public IActionResult ReadConfig()
+        {
+
+           return Json(new {
+                env = new { samplevar =_config.Get("samplevar") },
+                config = new {}
+            });
         }
 
         public User User()
--- Startup.cs
+++ Startup.cs
@@ -1,11 +1,25 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Routing;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Diagnostics;
 
 namespace KWebStartup
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            // Setup configuration sources.
+            Configuration = new Configuration()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+
+        }
+
         public void Configure(IApplicationBuilder app)
         {
             app.UseStaticFiles();
@@ -13,7 +27,11 @@ namespace KWebStartup
             app.UseServices(services =>
             {
                 services.AddMvc();
+                services.AddTransient<IConfiguration>(i => Configuration);
             });
+            
+            app.UseBrowserLink();
+            app.UseErrorPage(ErrorPageOptions.ShowAll);
 
             // Add MVC to the request pipeline
             app.UseMvc(routes =>
